d100f4dc289e4479e3f98e8dc3b955f76a55cdc7
PremDanej <danejprem005@gmail.com>
2024-03-22 16:27:14 +0530

    JetBizCard App Completed.

--- JetBizCard/app/src/main/java/com/merp/jet/biz/card/MainActivity.kt
+++ JetBizCard/app/src/main/java/com/merp/jet/biz/card/MainActivity.kt
@@ -2,25 +2,29 @@ package com.merp.jet.biz.card
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Divider
@@ -28,11 +32,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.merp.jet.biz.card.ui.theme.JetBizCardTheme
@@ -56,6 +64,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
+    val buttonClickedState = remember {
+        mutableStateOf(false)
+    }
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -64,15 +75,15 @@ fun CreateBizCard() {
         Card(
             modifier = Modifier
                 .padding(12.dp)
-                .fillMaxWidth()
-                .fillMaxHeight(),
+                .width(200.dp)
+                .height(390.dp),
             shape = RoundedCornerShape(corner = CornerSize(20.dp)),
             colors = CardDefaults.cardColors(Color.White),
             elevation = CardDefaults.cardElevation(4.dp)
         ) {
             Column(
                 modifier = Modifier
-                    .height(300.dp)
+                    .fillMaxHeight()
                     .fillMaxWidth(),
                 verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
@@ -80,11 +91,79 @@ fun CreateBizCard() {
                 CreateImageProfile()
                 Divider()
                 CreateInfo()
+                Button(
+                    onClick = {
+                        buttonClickedState.value = !buttonClickedState.value
+                    }, shape = RoundedCornerShape(0.dp)
+                ) {
+                    Text(text = "Portfolio")
+                }
+                if(buttonClickedState.value){
+                    Content()
+                }else{
+                    Box{
+
+                    }
+                }
             }
         }
 
     }
 
+}
+
+@Preview()
+@Composable
+fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Surface(modifier = Modifier
+            .padding(3.dp)
+            .fillMaxWidth()
+            .fillMaxHeight(),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            color = Color.White,
+            border = BorderStroke(2.dp, Color.LightGray)) {
+            Portfolio(data = listOf("Project 1", "Project 2", "Project 3", "Project 4", "Project 5"))
+
+        }
+    }
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn {
+        items(data){item ->
+            Card(modifier = Modifier
+                .padding(13.dp)
+                .fillMaxWidth(),
+                elevation = CardDefaults.cardElevation(2.dp),
+                colors = CardDefaults.cardColors(Color.White),
+                shape = RectangleShape) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(Color.White)
+                ) {
+                    CreateImageProfile(modifier = Modifier.size(100.dp))
+                    Column(modifier = Modifier.padding(7.dp).align(Alignment.CenterVertically )) {
+                        Text(
+                            text = item,
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = "A great Project",
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Composable
@@ -107,21 +186,12 @@ fun CreateInfo() {
         )
 
     }
-    Column(modifier = Modifier.padding(top = 10.dp)) {
-        Button(
-            onClick = {
-                Log.d("=======>", "Button clicked")
-            }, shape = RoundedCornerShape(0.dp)
-        ) {
-            Text(text = "Portfolio")
-        }
-    }
 }
 
 @Composable
 fun CreateImageProfile(modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .size(150.dp)
             .padding(5.dp),
         shape = CircleShape,
@@ -133,7 +203,7 @@ fun CreateImageProfile(modifier: Modifier = Modifier) {
         Image(
             painter = painterResource(id = R.drawable.profile_image),
             contentDescription = "profile image",
-            modifier = Modifier.size(135.dp),
+            modifier = modifier.size(135.dp),
             contentScale = ContentScale.Crop
         )
     }
