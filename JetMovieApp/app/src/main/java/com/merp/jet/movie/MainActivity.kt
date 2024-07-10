c9195c8d2ca81b9cdfaa5cd3b959df1cb329e019
PremDanej <danejprem005@gmail.com>
2024-07-10 17:52:28 +0530

    Displayed movie list.

--- JetMovieApp/app/src/main/java/com/merp/jet/movie/MainActivity.kt
+++ JetMovieApp/app/src/main/java/com/merp/jet/movie/MainActivity.kt
@@ -4,44 +4,150 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.merp.jet.movie.ui.theme.JetMovieTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JetMovieTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+            MyApp {
+                MainContent()
+            }
+        }
+    }
+}
+
+@ExperimentalMaterial3Api
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    JetMovieTheme {
+        Scaffold(
+            modifier = Modifier,
+            topBar = {
+                TopAppBar(
+                    title = { Text(text = "Movie") },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer
                     )
+                )
+            },
+            content = { paddingValues ->
+                Column(modifier = Modifier.padding(paddingValues)) {
+                    content()
                 }
+            }
+        )
+    }
+}
+
+@Composable
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Captain America",
+        "Iron Man",
+        "Thor",
+        "Iron Man 2",
+        "Avenger",
+        "Iron Man 3",
+        "Guardians of the Galaxy 1",
+        "Captain America Winter Soldier",
+        "Avenger Age of Ultron",
+        "Guardians of the Galaxy 2",
+        "Avenger Civil War",
+        "Avenger Infinity War",
+        "Avenger End Game",
+        "Thor Love and Thunder",
+        "Avenger Secret War"
+    )
+) {
+
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn() {
+            items(items = movieList) {
+                MovieRow(movie = it)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(horizontal = 4.dp, vertical = 10.dp)
+            .fillMaxWidth()
+            .height(130.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                tonalElevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+            }
+
+            Text(text = movie)
+        }
+    }
 }
 
+
+@ExperimentalMaterial3Api
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    JetMovieTheme {
-        Greeting("Android")
+    MyApp {
+        MainContent()
     }
+    /*JetMovieTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }*/
 }
