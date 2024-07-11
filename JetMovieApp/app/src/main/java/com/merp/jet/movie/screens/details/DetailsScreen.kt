c022a763a13c32e18e9713a80ad018ca3d10b66c
PremDanej <danejprem005@gmail.com>
2024-07-11 17:15:26 +0530

    Back navigation added.

--- JetMovieApp/app/src/main/java/com/merp/jet/movie/screens/details/DetailsScreen.kt
+++ JetMovieApp/app/src/main/java/com/merp/jet/movie/screens/details/DetailsScreen.kt
@@ -2,13 +2,17 @@ package com.merp.jet.movie.screens.details
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -19,6 +23,12 @@ fun DetailsScreen(navController: NavController, movieData: String?) {
             verticalArrangement = Arrangement.Center
         ) {
             Text(text = movieData.toString(), style = MaterialTheme.typography.titleLarge)
+            Spacer(modifier = Modifier.height(24.dp))
+            Button(onClick = {
+                navController.popBackStack()
+            }) {
+                Text(text = "Go Back")
+            }
         }
     }
 }
