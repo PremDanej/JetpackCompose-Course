3f7d63178dedc99acaa7c58618ed2e12aad61a43
PremDanej <danejprem005@gmail.com>
2024-07-13 02:12:01 +0530

    Add more movie info in Row.

--- JetMovieApp/app/src/main/java/com/merp/jet/movie/widgets/widgets.kt
+++ JetMovieApp/app/src/main/java/com/merp/jet/movie/widgets/widgets.kt
@@ -1,5 +1,6 @@
 package com.merp.jet.movie.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -7,13 +8,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
@@ -21,12 +22,21 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
 import com.merp.jet.movie.model.Movie
@@ -35,11 +45,13 @@ import com.merp.jet.movie.model.getMovies
 @Preview
 @Composable
 fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
     Card(
         modifier = Modifier
             .padding(horizontal = 4.dp, vertical = 10.dp)
             .fillMaxWidth()
-            .height(130.dp)
             .clickable {
                 onItemClick(movie.title)
             },
@@ -84,7 +96,29 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) 
                     text = "Released: ${movie.year}",
                     style = MaterialTheme.typography.bodySmall
                 )
-                // Text(text = movie.title)
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+                        Text( buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp)){
+                                append("Plot :")
+                            }
+                            withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp,
+                                fontWeight = FontWeight.Bold)){
+                                append(movie.plot)
+                            }
+                        })
+                    }
+                }
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Arrow Down",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        },
+                    tint = Color.DarkGray
+                )
             }
         }
     }
