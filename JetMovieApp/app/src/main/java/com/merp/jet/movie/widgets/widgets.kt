fe54c5af2b6c851e0cd43bbd0c2ac2735d474906
PremDanej <danejprem005@gmail.com>
2024-07-12 19:25:30 +0530

    Coil dependency added for images.

--- JetMovieApp/app/src/main/java/com/merp/jet/movie/widgets/widgets.kt
+++ JetMovieApp/app/src/main/java/com/merp/jet/movie/widgets/widgets.kt
@@ -1,7 +1,9 @@
 package com.merp.jet.movie.widgets
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,6 +17,7 @@ import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,18 +25,23 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
 import com.merp.jet.movie.model.Movie
+import com.merp.jet.movie.model.getMovies
 
+@Preview
 @Composable
-fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}) {
+fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) {
     Card(
         modifier = Modifier
             .padding(horizontal = 4.dp, vertical = 10.dp)
             .fillMaxWidth()
             .height(130.dp)
             .clickable {
-                onItemClick(movie.id)
+                onItemClick(movie.title)
             },
         colors = CardDefaults.cardColors(containerColor = Color.White),
         shape = RoundedCornerShape(corner = CornerSize(8.dp)),
@@ -53,10 +61,31 @@ fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}) {
                 shape = RectangleShape,
                 shadowElevation = 4.dp
             ) {
-                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+                Image(
+                    painter = rememberImagePainter(data = movie.poster, builder =
+                    {
+                        crossfade(true)
+                        transformations(CircleCropTransformation())
+                    }),
+                    contentDescription = "Image poster"
+                )
             }
 
-            Text(text = movie.title)
+            Column(modifier = Modifier.padding(4.dp)) {
+                Text(
+                    text = movie.title,
+                    style = MaterialTheme.typography.labelLarge
+                )
+                Text(
+                    text = "Director: ${movie.actors}",
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Text(
+                    text = "Released: ${movie.year}",
+                    style = MaterialTheme.typography.bodySmall
+                )
+                // Text(text = movie.title)
+            }
         }
     }
 }
