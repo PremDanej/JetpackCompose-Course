ff0401249d2efc9c5c1a4449a3aa3a01a10814f6
PremDanej <danejprem005@gmail.com>
2024-07-16 18:28:05 +0530

    Code optimize.

--- JetNoteApp/app/src/main/java/com/merp/jet/note/app/screen/NoteScreen.kt
+++ JetNoteApp/app/src/main/java/com/merp/jet/note/app/screen/NoteScreen.kt
@@ -1,11 +1,8 @@
 package com.merp.jet.note.app.screen
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Notifications
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -22,7 +19,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeOptions
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.merp.jet.note.app.R
