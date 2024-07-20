4fc32da31a62efbf0147b67a332bdd2eff439a75
PremDanej <danejprem005@gmail.com>
2024-07-20 15:13:00 +0530

    TypeConverters added.

--- JetNoteApp/app/src/main/java/com/merp/jet/note/app/data/NoteDatabase.kt
+++ JetNoteApp/app/src/main/java/com/merp/jet/note/app/data/NoteDatabase.kt
@@ -2,9 +2,13 @@ package com.merp.jet.note.app.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.merp.jet.note.app.model.Note
+import com.merp.jet.note.app.util.DateConverter
+import com.merp.jet.note.app.util.UUIDConverter
 
 @Database(entities = [Note::class], version = 1, exportSchema = false)
+@TypeConverters(DateConverter::class, UUIDConverter::class)
 abstract class NoteDatabase : RoomDatabase() {
-    abstract fun noteDao() : NoteDatabaseDao
+    abstract fun noteDao(): NoteDatabaseDao
 }
