f1f32c82c43b6ebd0c333f4e021b42a3e0fae1b3
PremDanej <danejprem005@gmail.com>
2024-07-31 18:29:33 +0530

    :star: Setting App Navigation - Navigation setup successfully.

---
+++ JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/navigation/WeatherScreens.kt
@@ -0,0 +1,10 @@
+package com.merp.jet.weather.forecast.app.navigation
+
+enum class WeatherScreens {
+    SplashScreen,
+    MainScreen,
+    AboutScreen,
+    FavoriteScreen,
+    SearchScreen,
+    SettingsScreen
+}
