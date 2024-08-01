ddb5f8f1e8d8cf6e43180602110110bc652d18eb
PremDanej <danejprem005@gmail.com>
2024-08-01 16:37:00 +0530

    SplashScreen animation and Navigate to MainScreen.

--- JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/navigation/WeatherNavigation.kt
+++ JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/navigation/WeatherNavigation.kt
@@ -4,14 +4,18 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.merp.jet.weather.forecast.app.screens.MainScreen
 import com.merp.jet.weather.forecast.app.screens.WeatherSplashScreen
 
 @Composable
 fun WeatherNavigation() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = WeatherScreens.SplashScreen.name) {
-        composable(WeatherScreens.SplashScreen.name){
+        composable(WeatherScreens.SplashScreen.name) {
             WeatherSplashScreen(navController = navController)
+        }
+        composable(WeatherScreens.MainScreen.name) {
+            MainScreen(navController = navController)
         }
     }
 }
--- JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/screens/WeatherSplashScreen.kt
+++ JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/screens/WeatherSplashScreen.kt
@@ -1,5 +1,8 @@
 package com.merp.jet.weather.forecast.app.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
@@ -11,20 +14,44 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.merp.jet.weather.forecast.app.R
+import com.merp.jet.weather.forecast.app.navigation.WeatherScreens
+import kotlinx.coroutines.delay
 
 @Composable
 fun WeatherSplashScreen(navController: NavController) {
+    val scale = remember {
+        Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(targetValue = 0.9f,
+            tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(8f).getInterpolation(it)
+                }
+            )
+        )
+        delay(2000L)
+        navController.navigate(route = WeatherScreens.MainScreen.name)
+    }
+
+
     Surface(
         modifier = Modifier
             .padding(15.dp)
+            .scale(scale.value)
             .size(300.dp),
         shape = CircleShape,
         color = Color.White,
