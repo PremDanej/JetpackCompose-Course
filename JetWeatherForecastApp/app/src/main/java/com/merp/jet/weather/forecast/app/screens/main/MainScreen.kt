4886d82d3562b591c0785c8739eab21a0b267adf
PremDanej <danejprem005@gmail.com>
2024-08-04 22:52:40 +0530

    Creating main screen.

--- JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/screens/main/MainScreen.kt
+++ JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/screens/main/MainScreen.kt
@@ -1,17 +1,24 @@
 package com.merp.jet.weather.forecast.app.screens.main
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -42,20 +49,36 @@ fun MainScaffold(weather: Weather, navController: NavController) {
                 title = weather.city.name + ", ${weather.city.country}",
                 icon = Icons.AutoMirrored.Default.ArrowBack,
                 navController = navController,
-            ){
+            ) {
                 Log.d("Back Btn ", "MainScaffold: CLicked")
             }
         }) { padding ->
-        Column(modifier = Modifier.padding(padding)) {
+        Column(
+            modifier = Modifier.padding(padding),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             HorizontalDivider(thickness = 0.5.dp)
-            MainContent(data = weather)
+            MainContent()
         }
     }
 }
 
+@Preview
 @Composable
-fun MainContent(data: Weather) {
-    Column {
-        Text(text = "${data.city}")
+fun MainContent() {
+    Column(
+        modifier = Modifier.padding(4.dp).fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "29, Mon",
+            style = MaterialTheme.typography.bodySmall,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(6.dp)
+                .fillMaxWidth()
+                .align(alignment = Alignment.End)
+        )
     }
 }
