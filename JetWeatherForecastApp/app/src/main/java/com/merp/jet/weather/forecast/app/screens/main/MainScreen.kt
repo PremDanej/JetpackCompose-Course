172b1ec483826cc353d327a5e1116edb304c5505
PremDanej <danejprem005@gmail.com>
2024-08-05 17:43:28 +0530

    Creating main Screen. Finalizing Circle and Data.

--- JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/screens/main/MainScreen.kt
+++ JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/screens/main/MainScreen.kt
@@ -1,29 +1,35 @@
 package com.merp.jet.weather.forecast.app.screens.main
 
 import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.merp.jet.weather.forecast.app.data.DataOrException
 import com.merp.jet.weather.forecast.app.model.Weather
+import com.merp.jet.weather.forecast.app.utils.Constants
+import com.merp.jet.weather.forecast.app.utils.formatDate
+import com.merp.jet.weather.forecast.app.utils.formatDecimals
 import com.merp.jet.weather.forecast.app.widgets.WeatherAppBar
 
 @Composable
@@ -47,7 +53,6 @@ fun MainScaffold(weather: Weather, navController: NavController) {
         topBar = {
             WeatherAppBar(
                 title = weather.city.name + ", ${weather.city.country}",
-                icon = Icons.AutoMirrored.Default.ArrowBack,
                 navController = navController,
             ) {
                 Log.d("Back Btn ", "MainScaffold: CLicked")
@@ -58,27 +63,57 @@ fun MainScaffold(weather: Weather, navController: NavController) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             HorizontalDivider(thickness = 0.5.dp)
-            MainContent()
+            MainContent(weather)
         }
     }
 }
 
-@Preview
 @Composable
-fun MainContent() {
+fun MainContent(data: Weather) {
+    val weatherItem = data.list[0]
+    val imageUrl = Constants.ICON_URL + "${weatherItem.weather[0].icon}.png"
     Column(
-        modifier = Modifier.padding(4.dp).fillMaxSize(),
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "29, Mon",
-            style = MaterialTheme.typography.bodySmall,
+            text = formatDate(weatherItem.dt),
+            style = MaterialTheme.typography.bodyLarge,
             fontWeight = FontWeight.Bold,
             modifier = Modifier
                 .padding(6.dp)
-                .fillMaxWidth()
-                .align(alignment = Alignment.End)
         )
+        Surface(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .padding(4.dp)
+                .size(200.dp),
+            shape = CircleShape,
+            color = Color(0xFFFFC400)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                WeatherStateImage(imageUrl = imageUrl)
+                Text(
+                    text = formatDecimals(weatherItem.temp.day) + "°",
+                    style = MaterialTheme.typography.headlineLarge,
+                    fontWeight = FontWeight.ExtraBold
+                )
+                Text(text = weatherItem.weather[0].main, fontStyle = FontStyle.Italic)
+            }
+        }
     }
+}
+
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+    Image(
+        painter = rememberImagePainter(imageUrl), contentDescription = "Icon Image",
+        modifier = Modifier.size(80.dp)
+    )
 }
--- JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/utils/Constants.kt
+++ JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/utils/Constants.kt
@@ -3,4 +3,5 @@ package com.merp.jet.weather.forecast.app.utils
 object Constants {
     const val BASE_URL : String = "https://api.openweathermap.org/"
     const val API_KEY : String = ""
+    const val ICON_URL : String = "https://openweathermap.org/img/wn/"
 }
