c61757993780db62ab2c97d419ac7b6df4c3eefc
PremDanej <danejprem005@gmail.com>
2024-08-06 01:59:15 +0530

    Added Sunset and Sunrise row.

--- JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/screens/main/MainScreen.kt
+++ JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/screens/main/MainScreen.kt
@@ -4,12 +4,14 @@ import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -19,16 +21,20 @@ import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
+import com.merp.jet.weather.forecast.app.R
 import com.merp.jet.weather.forecast.app.data.DataOrException
 import com.merp.jet.weather.forecast.app.model.Weather
+import com.merp.jet.weather.forecast.app.model.WeatherItem
 import com.merp.jet.weather.forecast.app.utils.Constants
 import com.merp.jet.weather.forecast.app.utils.formatDate
+import com.merp.jet.weather.forecast.app.utils.formatDateTime
 import com.merp.jet.weather.forecast.app.utils.formatDecimals
 import com.merp.jet.weather.forecast.app.widgets.WeatherAppBar
 
@@ -106,6 +112,98 @@ fun MainContent(data: Weather) {
                 )
                 Text(text = weatherItem.weather[0].main, fontStyle = FontStyle.Italic)
             }
+        }
+        HumidityWindPressureRow(weather = weatherItem)
+        HorizontalDivider(thickness = 0.5.dp)
+        DayNightRow(weather = weatherItem)
+    }
+}
+
+@Composable
+fun DayNightRow(weather: WeatherItem) {
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        Row(
+            modifier = Modifier.padding(4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.sunrise),
+                contentDescription = "SunRise Icon",
+                modifier = Modifier
+                    .size(25.dp)
+                    .padding(end = 5.dp)
+            )
+            Text(
+                text = formatDateTime(weather.sunrise),
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        Row(
+            modifier = Modifier.padding(4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = formatDateTime(weather.sunset),
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.sunset),
+                contentDescription = "SunSet Icon",
+                modifier = Modifier
+                    .size(25.dp)
+                    .padding(start = 5.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun HumidityWindPressureRow(weather: WeatherItem) {
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Row(modifier = Modifier.padding(4.dp)) {
+            Icon(
+                painter = painterResource(id = R.drawable.humidity),
+                contentDescription = "Humidity Icon",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(
+                text = "${weather.humidity}%",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        Row(modifier = Modifier.padding(4.dp)) {
+            Icon(
+                painter = painterResource(id = R.drawable.pressure),
+                contentDescription = "Pressure Icon",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(
+                text = "${weather.pressure} psi",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+        Row(modifier = Modifier.padding(4.dp)) {
+            Icon(
+                painter = painterResource(id = R.drawable.wind),
+                contentDescription = "Wind Icon",
+                modifier = Modifier.size(20.dp)
+            )
+            Text(
+                text = "${weather.humidity} mph",
+                style = MaterialTheme.typography.bodyMedium
+            )
         }
     }
 }
