4d9b62f4a89a2f47de8630cb5279191ed10fed39
PremDanej <danejprem005@gmail.com>
2024-08-09 19:01:17 +0530

    3 new screens added.

---
+++ JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/screens/about/AboutScreen.kt
@@ -0,0 +1,10 @@
+package com.merp.jet.weather.forecast.app.screens.about
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun AboutScreen(navController: NavController) {
+    Text(text = "AboutUs")
+}
