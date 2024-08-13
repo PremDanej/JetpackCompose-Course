8d6c9692d0c376944adf55a2ae229055845a5363
PremDanej <danejprem005@gmail.com>
2024-08-13 12:11:29 +0530

    Database Dependency Injection added in AppModule.

--- JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/di/AppModule.kt
+++ JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.merp.jet.weather.forecast.app.di
 
+import android.content.Context
+import androidx.room.Room
+import com.merp.jet.weather.forecast.app.data.WeatherDao
+import com.merp.jet.weather.forecast.app.data.WeatherDatabase
 import com.merp.jet.weather.forecast.app.network.WeatherApi
 import com.merp.jet.weather.forecast.app.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -13,6 +18,18 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideWeatherDao(weatherDatabase: WeatherDatabase): WeatherDao =
+        weatherDatabase.weatherDao()
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): WeatherDatabase =
+        Room.databaseBuilder(context, WeatherDatabase::class.java, "weather_database")
+            .fallbackToDestructiveMigration()
+            .build()
 
     @Provides
     @Singleton
