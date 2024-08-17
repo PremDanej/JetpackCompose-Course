aacc583284e3b59dcb3747c3d2e01f9b7f10be3a
PremDanej <danejprem005@gmail.com>
2024-08-18 01:18:28 +0530

    SettingScreen UI updated.

--- JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/screens/setting/SettingsScreen.kt
+++ JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/screens/setting/SettingsScreen.kt
@@ -1,10 +1,85 @@
 package com.merp.jet.weather.forecast.app.screens.setting
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.merp.jet.weather.forecast.app.R
+import com.merp.jet.weather.forecast.app.widgets.WeatherAppBar
 
 @Composable
-fun SettingsScreen(navController: NavController) {
-    Text(text = "Setting")
+fun SettingsScreen(
+    navController: NavController,
+    settingsViewModel: SettingsViewModel = hiltViewModel()
+) {
+
+    var unitToggleState by remember {
+        mutableStateOf(false)
+    }
+    val measurementUnits = listOf("Imperial (F)", "Metric (C)")
+    var choiceState by remember {
+        mutableStateOf("")
+    }
+
+    Scaffold(topBar = {
+        WeatherAppBar(
+            navController = navController,
+            icon = Icons.AutoMirrored.Default.ArrowBack,
+            onButtonClicked = { navController.popBackStack() },
+            iSMainScreen = false
+        )
+    }) { paddingValues ->
+        Surface(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(15.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.lbl_change_measurement),
+                    modifier = Modifier.padding(bottom = 15.dp)
+                )
+
+                IconToggleButton(
+                    checked = !unitToggleState, onCheckedChange = {
+                        unitToggleState = !it
+                        choiceState = (if (unitToggleState) "Imperial (F)" else "Metric (C)")
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth(0.5f)
+                        .clip(shape = RectangleShape)
+                        .padding(5.dp)
+                        .background(Color.Magenta.copy(alpha = 0.4f))
+                ) {
+                    Text(text = if (unitToggleState) "Fahrenheit °F" else " Celsius °C")
+                }
+            }
+        }
+    }
 }
