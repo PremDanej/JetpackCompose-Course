d1ae4dfa90e86b6596c0ad1d67f007dcde41e866
PremDanej <danejprem005@gmail.com>
2024-08-18 00:35:56 +0530

    CRUD functions are added in WeatherDao.

--- JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/data/WeatherDao.kt
+++ JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/data/WeatherDao.kt
@@ -1,13 +1,14 @@
 package com.merp.jet.weather.forecast.app.data
 
- import androidx.room.Dao
- import androidx.room.Delete
- import androidx.room.Insert
- import androidx.room.OnConflictStrategy
- import androidx.room.Query
- import androidx.room.Update
- import com.merp.jet.weather.forecast.app.model.Favorite
- import kotlinx.coroutines.flow.Flow
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.merp.jet.weather.forecast.app.model.Favorite
+import com.merp.jet.weather.forecast.app.model.Unit
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface WeatherDao {
@@ -16,7 +17,7 @@ interface WeatherDao {
     fun getFavorites(): Flow<List<Favorite>>
 
     @Query(value = "SELECT * FROM fav_tbl WHERE city =:city")
-    suspend fun getFavById(city: String) : Favorite
+    suspend fun getFavById(city: String): Favorite
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertFavorite(favorite: Favorite)
@@ -29,4 +30,21 @@ interface WeatherDao {
 
     @Delete
     suspend fun deleteFavorite(favorite: Favorite)
+
+    // Unit table
+
+    @Query(value = "SELECT * FROM settings_tbl")
+    fun getUnits(): Flow<List<Unit>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUnit(unit: Unit)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateUnit(unit: Unit)
+
+    @Query(value = "DELETE FROM settings_tbl")
+    suspend fun deleteAllUnits()
+
+    @Delete
+    suspend fun deleteUnit(unit: Unit)
 }
