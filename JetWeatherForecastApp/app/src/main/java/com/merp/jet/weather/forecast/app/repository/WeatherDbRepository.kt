b4fa7f3d337ebcce5209370af177cf90a40269bb
PremDanej <danejprem005@gmail.com>
2024-08-18 00:37:37 +0530

    Unit functions are added to the WeatherDbRepository.

--- JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/repository/WeatherDbRepository.kt
+++ JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/repository/WeatherDbRepository.kt
@@ -2,6 +2,7 @@ package com.merp.jet.weather.forecast.app.repository
 
 import com.merp.jet.weather.forecast.app.data.WeatherDao
 import com.merp.jet.weather.forecast.app.model.Favorite
+import com.merp.jet.weather.forecast.app.model.Unit
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
@@ -13,4 +14,10 @@ class WeatherDbRepository @Inject constructor(private val weatherDao: WeatherDao
     suspend fun deleteAllFavorite() = weatherDao.deleteAllFavorite()
     suspend fun deleteFavorite(favorite: Favorite) = weatherDao.deleteFavorite(favorite)
     suspend fun getFavById(city: String) = weatherDao.getFavById(city)
+
+    fun getUnits(): Flow<List<Unit>> = weatherDao.getUnits()
+    suspend fun insertUnit(unit: Unit) = weatherDao.insertUnit(unit)
+    suspend fun updateUnit(unit: Unit) = weatherDao.updateUnit(unit)
+    suspend fun deleteAllUnits() = weatherDao.deleteAllUnits()
+    suspend fun deleteUnit(unit: Unit) = weatherDao.deleteUnit(unit)
 }
