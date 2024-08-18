dfeaaa2f370b46fef14d17e976865283e8d0a248
PremDanej <danejprem005@gmail.com>
2024-08-18 17:53:44 +0530

    Saving measurement Units to the database.

--- JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/screens/setting/SettingsScreen.kt
+++ JetWeatherForecastApp/app/src/main/java/com/merp/jet/weather/forecast/app/screens/setting/SettingsScreen.kt
@@ -1,18 +1,24 @@
 package com.merp.jet.weather.forecast.app.screens.setting
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,9 +30,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.merp.jet.weather.forecast.app.R
+import com.merp.jet.weather.forecast.app.model.Unit
 import com.merp.jet.weather.forecast.app.widgets.WeatherAppBar
 
 @Composable
@@ -45,6 +53,7 @@ fun SettingsScreen(
 
     Scaffold(topBar = {
         WeatherAppBar(
+            title = "Settings",
             navController = navController,
             icon = Icons.AutoMirrored.Default.ArrowBack,
             onButtonClicked = { navController.popBackStack() },
@@ -78,6 +87,27 @@ fun SettingsScreen(
                         .background(Color.Magenta.copy(alpha = 0.4f))
                 ) {
                     Text(text = if (unitToggleState) "Fahrenheit °F" else " Celsius °C")
+                }
+
+                Button(
+                    onClick = {
+                        settingsViewModel.deleteAllUnits()
+                        settingsViewModel.insertUnit(Unit(choiceState))
+                    },
+                    modifier = Modifier
+                        .padding(10.dp),
+                    shape = RoundedCornerShape(34.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = MaterialTheme.colorScheme.onBackground
+                    ),
+                    border = BorderStroke(0.5.dp, MaterialTheme.colorScheme.onBackground)
+                ) {
+                    Text(
+                        text = "Save",
+                        modifier = Modifier.padding(horizontal = 10.dp),
+                        fontSize = 18.sp
+                    )
                 }
             }
         }
