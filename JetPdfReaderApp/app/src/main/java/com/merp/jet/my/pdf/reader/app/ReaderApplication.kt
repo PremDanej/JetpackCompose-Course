764503f9f734c69e438daca81d986bb10e7527cd
PremDanej <danejprem005@gmail.com>
2024-08-23 18:32:04 +0530

    Setting up DI with Hilt.

---
+++ JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/ReaderApplication.kt
@@ -0,0 +1,7 @@
+package com.merp.jet.my.pdf.reader.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ReaderApplication : Application()
---
+++ JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/di/AppModule.kt
@@ -0,0 +1,11 @@
+package com.merp.jet.my.pdf.reader.app.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    // Add provider
+}
