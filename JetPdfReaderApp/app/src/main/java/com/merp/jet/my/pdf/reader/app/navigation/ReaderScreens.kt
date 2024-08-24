453b2981b2ef7ec3f429c9525483bc3b67b0df53
PremDanej <danejprem005@gmail.com>
2024-08-24 16:18:52 +0530

    Setting Up Screen Enum.

---
+++ JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/navigation/ReaderScreens.kt
@@ -0,0 +1,27 @@
+package com.merp.jet.my.pdf.reader.app.navigation
+
+enum class ReaderScreens {
+    SplashScreen,
+    LoginScreen,
+    CreateAccountScreen,
+    HomeScreen,
+    SearchScreen,
+    UpdateScreen,
+    DetailsScreen,
+    StatsScreen;
+
+    companion object {
+        fun fromRoute(route: String): ReaderScreens = when (route?.substringBefore("/")) {
+            SplashScreen.name -> SplashScreen
+            LoginScreen.name -> LoginScreen
+            CreateAccountScreen.name -> CreateAccountScreen
+            HomeScreen.name -> HomeScreen
+            SearchScreen.name -> SearchScreen
+            StatsScreen.name -> StatsScreen
+            UpdateScreen.name -> UpdateScreen
+            DetailsScreen.name -> DetailsScreen
+            null -> HomeScreen
+            else -> throw IllegalArgumentException("Route $route is not recognised ")
+        }
+    }
+}
