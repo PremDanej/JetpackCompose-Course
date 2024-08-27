a0ec4d00f7b28677a89dca24403a7a5348ed5467
PremDanej <danejprem005@gmail.com>
2024-08-28 01:44:53 +0530

    Animating SplashScreen.

--- JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/screens/splash/ReaderSplashScreen.kt
+++ JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/screens/splash/ReaderSplashScreen.kt
@@ -1,5 +1,8 @@
 package com.merp.jet.my.pdf.reader.app.screens.splash
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,16 +16,39 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.merp.jet.my.pdf.reader.app.R
+import com.merp.jet.my.pdf.reader.app.navigation.ReaderScreens
+import kotlinx.coroutines.delay
 
 @Composable
 fun SplashScreen(navController: NavController) {
+    val scale = remember {
+        Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            0.9f,
+            tween(
+                800,
+                easing = {
+                    OvershootInterpolator(8f)
+                        .getInterpolation(it)
+                },
+            )
+        )
+        delay(2000L)
+    }
+
     Column(
         Modifier.fillMaxSize(),
         Arrangement.Center, Alignment.CenterHorizontally,
@@ -30,7 +56,8 @@ fun SplashScreen(navController: NavController) {
         Surface(
             Modifier
                 .padding(15.dp)
-                .size(300.dp),
+                .size(300.dp)
+                .scale(scale.value),
             border = BorderStroke(2.dp, Color.LightGray),
             shape = CircleShape,
             color = MaterialTheme.colorScheme.background
