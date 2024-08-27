480a8c12dad04398fa35cc2d08570e82666b42f8
PremDanej <danejprem005@gmail.com>
2024-08-28 01:39:06 +0530

    MainActivity.kt code updated.

--- JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/MainActivity.kt
+++ JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/MainActivity.kt
@@ -4,14 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.merp.jet.my.pdf.reader.app.navigation.ReaderNavigation
 import com.merp.jet.my.pdf.reader.app.ui.theme.PDFReaderTheme
@@ -34,16 +30,9 @@ class MainActivity : ComponentActivity() {
 fun ReaderApp() {
     Scaffold { paddingValues ->
         Surface(
-            Modifier
-                .padding(paddingValues)
-                .fillMaxSize()
+            Modifier.padding(paddingValues)
         ) {
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                ReaderNavigation()
-            }
+            ReaderNavigation()
         }
     }
 }
