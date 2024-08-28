e873d432d9c011a2204ee2e0e38aba8497543879
PremDanej <danejprem005@gmail.com>
2024-08-28 17:05:08 +0530

    New font and Typography style added.

--- JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/ui/theme/Type.kt
+++ JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/ui/theme/Type.kt
@@ -2,33 +2,29 @@ package com.merp.jet.my.pdf.reader.app.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.merp.jet.my.pdf.reader.app.R.font.grotesk
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    headlineLarge = TextStyle(
+        fontFamily = FontFamily(Font(grotesk)),
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
+        fontSize = 34.sp,
+        lineHeight = 40.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = FontFamily(Font(grotesk)),
+        fontWeight = FontWeight.Medium,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
+
+// If not using typography then using this baseFont
+val baseFont = FontFamily(Font(grotesk, FontWeight.Normal))
