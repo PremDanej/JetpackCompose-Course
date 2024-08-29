9575df68a9b5c2d7ff5e572cbf35f84214a4207f
PremDanej <danejprem005@gmail.com>
2024-08-30 01:36:19 +0530

    Button TextStyle added.

--- JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/ui/theme/Type.kt
+++ JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/ui/theme/Type.kt
@@ -8,25 +8,28 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.merp.jet.my.pdf.reader.app.R.font.grotesk
 
+val baseFont = FontFamily(Font(grotesk))
+
 // Set of Material typography styles to start with
 val Typography = Typography(
     headlineLarge = TextStyle(
-        fontFamily = FontFamily(Font(grotesk)),
+        fontFamily = baseFont,
         fontWeight = FontWeight.Normal,
         fontSize = 34.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.5.sp
     ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily(Font(grotesk)),
+        fontFamily = baseFont,
         fontWeight = FontWeight.Medium,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
+
     // InputText value style
     bodyLarge = TextStyle(
-        fontFamily = FontFamily(Font(grotesk)),
+        fontFamily = baseFont,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
@@ -34,13 +37,19 @@ val Typography = Typography(
     ),
     // InputText label style
     bodySmall = TextStyle(
-        fontFamily = FontFamily(Font(grotesk)),
+        fontFamily = baseFont,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
-)
 
-// If not using typography then using this baseFont
-val baseFont = FontFamily(Font(grotesk, FontWeight.Normal))
+    // Button text style
+    labelLarge = TextStyle(
+        fontFamily = baseFont,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.sp
+    ),
+)
