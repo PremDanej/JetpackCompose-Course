f2347685125596dbdfd29f464f8ce3b891d794da
PremDanej <danejprem005@gmail.com>
2024-09-03 23:20:49 +0530

    Update home UI.

--- JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/screens/home/ReaderHomeScreen.kt
+++ JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/screens/home/ReaderHomeScreen.kt
@@ -1,9 +1,65 @@
 package com.merp.jet.my.pdf.reader.app.screens.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun HomeScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            ReaderAppBar(title = "",
+                showProfile = false,
+                navController = navController,
+                )
+        },
+        floatingActionButton = {
+            FABContent {}
+        }
 
+    ) { padding ->
+        Surface(Modifier.padding(padding)) {
+            Column(
+                Modifier
+                    .fillMaxSize(),
+                Arrangement.Center,
+                Alignment.CenterHorizontally
+            ) {
+                Text(text = "HomeScreen")
+            }
+        }
+    }
+}
+
+
+@Composable
+fun FABContent(onTap: (String) -> Unit) {
+    FloatingActionButton(
+        onClick = { onTap("") },
+        shape = RoundedCornerShape(50.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = "Add a Book",
+            tint = MaterialTheme.colorScheme.onSecondary
+        )
+    }
 }
