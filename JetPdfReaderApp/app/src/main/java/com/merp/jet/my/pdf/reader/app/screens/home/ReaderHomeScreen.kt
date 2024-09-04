ef93c698f1f21325b999fa6241e0658af1c16e6d
PremDanej <danejprem005@gmail.com>
2024-09-04 17:39:14 +0530

    Created TopAppbar - Logo and Title.

--- JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/screens/home/ReaderHomeScreen.kt
+++ JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/screens/home/ReaderHomeScreen.kt
@@ -1,54 +1,106 @@
 package com.merp.jet.my.pdf.reader.app.screens.home
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ExitToApp
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.merp.jet.my.pdf.reader.app.R
 
 @Composable
 fun HomeScreen(navController: NavController) {
     Scaffold(
         topBar = {
-            ReaderAppBar(title = "",
-                showProfile = false,
+            ReaderAppBar(
+                title = stringResource(id = R.string.app_name),
+                showProfile = true,
                 navController = navController,
-                )
+            )
         },
         floatingActionButton = {
             FABContent {}
         }
-
     ) { padding ->
         Surface(Modifier.padding(padding)) {
             Column(
                 Modifier
-                    .fillMaxSize(),
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
                 Arrangement.Center,
                 Alignment.CenterHorizontally
             ) {
-                Text(text = "HomeScreen")
+                Text(
+                    text = "HomeScreen",
+                    style = MaterialTheme.typography.headlineLarge,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ReaderAppBar(
+    title: String,
+    showProfile: Boolean = true,
+    navController: NavController
+) {
+    TopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                if (showProfile) {
+                    Icon(imageVector = Icons.Default.Face, contentDescription = "Profile")
+                    Spacer(modifier = Modifier.width(10.dp))
+                }
+                Text(
+                    text = title,
+                    color = Color.Red.copy(0.8f),
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Transparent),
+        actions = {
+            if (showProfile) {
+                IconButton(onClick = { /*TODO add navigation*/ }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Default.ExitToApp,
+                        contentDescription = "Sign out",
+                        tint = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            }
+        }
+    )
+
+}
 
 @Composable
 fun FABContent(onTap: (String) -> Unit) {
@@ -59,7 +111,6 @@ fun FABContent(onTap: (String) -> Unit) {
         Icon(
             imageVector = Icons.Default.Add,
             contentDescription = "Add a Book",
-            tint = MaterialTheme.colorScheme.onSecondary
         )
     }
 }
