611d4d07b6e76f4406c0ac989cead3333a915d12
PremDanej <danejprem005@gmail.com>
2024-09-05 23:25:24 +0530

    Book data class added.

---
+++ JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/model/MBook.kt
@@ -0,0 +1,8 @@
+package com.merp.jet.my.pdf.reader.app.model
+
+data class MBook(
+    var id: String? = null,
+    var title: String? = null,
+    var authors: String? = null,
+    var notes: String? = null,
+)
