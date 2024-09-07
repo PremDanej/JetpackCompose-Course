bf822d94c545ca1cc589cdc983ba97dd59c15c69
PremDanej <danejprem005@gmail.com>
2024-09-07 17:08:15 +0530

    Added 3 components to the Components.kt

--- JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/components/Components.kt
+++ JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/components/Components.kt
@@ -1,19 +1,33 @@
 package com.merp.jet.my.pdf.reader.app.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Logout
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -22,7 +36,11 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
 import com.merp.jet.my.pdf.reader.app.R
+import com.merp.jet.my.pdf.reader.app.navigation.ReaderScreens
 
 @Composable
 fun ReaderLogo(modifier: Modifier = Modifier) {
@@ -120,4 +138,67 @@ fun PasswordVisibility(passwordVisibility: MutableState<Boolean>) {
             contentDescription = "Password"
         )
     }
+}
+
+@Composable
+fun FABContent(onTap: (String) -> Unit) {
+    FloatingActionButton(
+        onClick = { onTap("") },
+        shape = RoundedCornerShape(50.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Default.Add,
+            contentDescription = "Add a Book",
+        )
+    }
+}
+
+@Composable
+fun TitleSection(
+    modifier: Modifier = Modifier,
+    label: String = stringResource(R.string.app_name)
+) {
+    Surface(modifier.padding(5.dp)) {
+        Column {
+            Text(text = label, style = MaterialTheme.typography.bodyLarge, fontSize = 20.sp)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ReaderAppBar(
+    title: String,
+    showProfile: Boolean = true,
+    navController: NavController
+) {
+    TopAppBar(
+        title = {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                if (showProfile) {
+                    Icon(imageVector = Icons.Default.Face, contentDescription = "Profile")
+                    Spacer(modifier = Modifier.width(10.dp))
+                }
+                Text(
+                    text = title,
+                    color = Color.Red.copy(0.8f),
+                    style = MaterialTheme.typography.titleLarge
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Transparent),
+        actions = {
+            IconButton(onClick = {
+                FirebaseAuth.getInstance().signOut().run {
+                    navController.navigate(ReaderScreens.LoginScreen.name)
+                }
+            }) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Default.Logout,
+                    contentDescription = "Sign out",
+                    tint = MaterialTheme.colorScheme.onBackground
+                )
+            }
+        }
+    )
 }
