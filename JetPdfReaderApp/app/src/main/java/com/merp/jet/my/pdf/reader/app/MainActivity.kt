cdf359c8cc6cc0c46bb8389cf9e30f4332c14f15
PremDanej <danejprem005@gmail.com>
2024-09-07 17:00:43 +0530

    Remove Scaffold from MainActivity.

--- JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/MainActivity.kt
+++ JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/MainActivity.kt
@@ -4,11 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.merp.jet.my.pdf.reader.app.navigation.ReaderNavigation
 import com.merp.jet.my.pdf.reader.app.ui.theme.PDFReaderTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -28,11 +25,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ReaderApp() {
-    Scaffold { paddingValues ->
-        Surface(
-            Modifier.padding(paddingValues)
-        ) {
-            ReaderNavigation()
-        }
+    Surface {
+        ReaderNavigation()
     }
 }
