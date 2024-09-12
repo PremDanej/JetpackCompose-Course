b687d3380831835e250f94f7913b206835607d09
PremDanej <danejprem005@gmail.com>
2024-09-12 17:10:28 +0530

    ApiProvider added in AppModule.

--- JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/di/AppModule.kt
+++ JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/di/AppModule.kt
@@ -1,11 +1,28 @@
 package com.merp.jet.my.pdf.reader.app.di
 
+import com.merp.jet.my.pdf.reader.app.network.BooksApi
+import com.merp.jet.my.pdf.reader.app.utils.Constants
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
     // Add provider
+
+    @Singleton
+    @Provides
+    fun provideBookApi(): BooksApi {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(BooksApi::class.java)
+    }
 }
