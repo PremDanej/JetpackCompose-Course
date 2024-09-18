436d19b76bef40721b500fa937f831baf044bb1b
PremDanej <danejprem005@gmail.com>
2024-09-18 17:43:01 +0530

    BookViewModel updated.

--- JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/screens/search/BookSearchViewModel.kt
+++ JetPdfReaderApp/app/src/main/java/com/merp/jet/my/pdf/reader/app/screens/search/BookSearchViewModel.kt
@@ -1,13 +1,16 @@
 package com.merp.jet.my.pdf.reader.app.screens.search
 
-import androidx.compose.runtime.MutableState
+import android.util.Log
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.merp.jet.my.pdf.reader.app.data.DataOrException
+import com.merp.jet.my.pdf.reader.app.data.Resource
 import com.merp.jet.my.pdf.reader.app.model.Item
 import com.merp.jet.my.pdf.reader.app.repository.BookRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,22 +18,33 @@ import javax.inject.Inject
 class BookSearchViewModel @Inject constructor(private val repository: BookRepository) :
     ViewModel() {
 
-    val listOfBooks: MutableState<DataOrException<List<Item>, Boolean, Exception>> =
-        mutableStateOf(DataOrException(listOf(), true, Exception("")))
+    var listOfBooks: List<Item> by mutableStateOf(listOf())
 
     init {
+        loadBooks()
+    }
+
+    private fun loadBooks() {
         searchBooks("android")
     }
 
-    internal fun searchBooks(query: String) {
-        viewModelScope.launch {
-            if (query.isEmpty()) {
-                return@launch
-            }
-            listOfBooks.value.loading = true
-            listOfBooks.value = repository.getBooks(query)
-            if (listOfBooks.value.data!!.isNotEmpty()) {
-                listOfBooks.value.loading = false
+    private fun searchBooks(query: String) {
+        viewModelScope.launch(Dispatchers.Default) {
+            if (query.isEmpty()) return@launch
+            try {
+                when (val response = repository.getBooks(query)) {
+                    is Resource.Success -> {
+                        listOfBooks = response.data!!
+                    }
+
+                    is Resource.Error -> {
+                        Log.e("TAG", "searchBooks: Failed getting books")
+                    }
+
+                    else -> {}
+                }
+            } catch (e: Exception) {
+                Log.d("TAG", "searchBooks: ${e.message}")
             }
         }
     }
